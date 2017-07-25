edc1bc86ccce8eb75e29d7d693a7f89533ab2764
Alexandru Budesteanu <Alexandru.Budesteanu@lairdtech.com>
2017-07-25 16:31:20 +0200

    implemnted folder sync and removed some junk

--- remote_file_sync/handlers.py
+++ remote_file_sync/handlers.py
@@ -4,31 +4,36 @@ import shlex
 from scp import SCPClient
 from watchdog.events import FileSystemEventHandler, PatternMatchingEventHandler, FileModifiedEvent
 from subprocess import check_call, DEVNULL
+from functools import reduce
 
 class DirWatcher(FileSystemEventHandler):
 
-    def __init__(self, ssh, root_directory, destination):
+    def __init__(self, ssh, root_directory, remote_location):
         super(DirWatcher, self).__init__()
         self._can_send = True
         self._root_directory = root_directory
-        self._destination = destination
+        self._remote_location = remote_location
         self._sftp = ssh.open_sftp()
+        # map folders and files here so that when a file
+        # will modify you will not get a file doesn't exist error
+        self._map_structure()
 
     def reset_lock(self):
         self._can_send = True
 
+    def _map_structure(self):
+        # for simplicity, we will use rsync.
+        rsync_command = "rsync -a {} {}".format(self._root_directory, self._remote_location)
+        check_call(rsync_command, stdout=DEVNULL, stderr=DEVNULL, shell=True)
+
     def _sync(self, path):
-        directory, file = os.path.split(path)
-        subdirs = directory.split("/")[1:] if "/" in directory else ['']
-        print("dirname: {}".format(subdirs))
-        print("filename: {}".format(file))
-        f_path = None
-        for dir in subdirs:
-            f_path = "/".join(dir)
-        f_path += "/"+file
-        print(f_path)
-        self._sftp.put(path, self._destination+f_path)
-        logging.info("Folder synced successfully")
+        # if the remote_location contains a / we must map a remote root, otherwise the root will be ~ (user home)
+        remote_root_folder = self._remote_location.split('/')[-1] if '/' in self._remote_location else None
+        partial_path = reduce(lambda final_path, partial_path: final_path + '/' + partial_path, path.split('/')[1:])
+        remote_path = remote_root_folder + '/' + partial_path if remote_root_folder else partial_path
+        logging.info(remote_path)
+        self._sftp.put(path, remote_path)
+        logging.info("{} synced successfully".format(path))
 
     def on_modified(self, event):
         # 'and' clause added because of the way some editors handle file edits
@@ -52,7 +57,7 @@ class FileWatcher(PatternMatchingEventHandler):
     def _sync(self, file):
         with SCPClient(self._ssh_client.get_transport()) as scp:
             scp.put(file, self._destination)
-        logging.info("File synced successfully")
+        logging.info("{} synced successfully".format(file))
 
     def on_modified(self, event):
         super(FileWatcher, self).on_modified(event)
